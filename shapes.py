52a3fc6d99fc3cfd9b77ec65a422bf1e669a851e
waithiageni <mwangiwaithiageni33@gmail.com>
2019-04-30 19:40:03 +0300

    added example5

---
+++ shapes.py
@@ -0,0 +1,69 @@
+class Circle:
+	def __init__(self,radius):
+		self.radius=radius
+		
+
+	def Area_of_a_circle(self,r):
+		r=self.radius
+		A=22/7(14*14)
+
+	def circumference_of_a_circle(self,c):
+		c=self.radius
+		C=2*3.142*c
+				
+
+class Square:
+	def __init__(self,side):
+		self.side=0
+
+	def Area_of_a_square(self,a):
+		a=self.side
+		Area_of_a_square=a*a
+
+	def Perimeter_of_a_circle(self,p):
+		p=self.side
+		Perimeter_of_a_circle=4(p)
+
+
+class Rectangle:
+	def __init__(self,w,l):
+		self.w=0
+		self.l=0		
+
+
+	def Area_of_a_rectangle(self,m,n):
+		m=self.w
+		n=self.l
+		A=m*n
+
+	def Perimeter_of_a_rectangle(self,q,s):
+		q=self.w
+		s=sel.l
+		Perimeter_of_a_rectangle=2(q+s)
+
+
+class Sphere:
+	def __init__(self,sphere_radius):
+		self.sphere_radius=0
+
+	def Sphere_SA(self,t):
+		t=self.sphere_radius
+		Sphere_SA=4*3.142*t*t
+
+	def Sphere_Volume(self,v):
+		v=self.sphere_radius
+		Sphere_Volume=3/4(3.142*v*v*v)	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
