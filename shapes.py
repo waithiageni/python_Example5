43de704c0427feb0719f05fe9c99b68e690cf998
waithiageni <mwangiwaithiageni33@gmail.com>
2019-05-01 18:28:47 +0300

    example5

--- shapes.py
+++ shapes.py
@@ -3,56 +3,64 @@ class Circle:
 		self.radius=radius
 		
 
-	def Area_of_a_circle(self,r):
-		r=self.radius
-		A=22/7(14*14)
+	def Area_of_a_circle(self):
+		radius=self.radius
+		A=(3.142)*(radius*radius)
+		return(A)
 
-	def circumference_of_a_circle(self,c):
-		c=self.radius
-		C=2*3.142*c
+	def circumference_of_a_circle(self):
+		radius=self.radius
+		C=2*3.142*(radius)
+		return(C)
 				
 
 class Square:
 	def __init__(self,side):
-		self.side=0
+		self.side=side
 
-	def Area_of_a_square(self,a):
-		a=self.side
-		Area_of_a_square=a*a
+	def Area_of_a_square(self):
+		side=self.side
+		Area_of_a_square=side*side
+		return(Area_of_a_square)
 
-	def Perimeter_of_a_circle(self,p):
-		p=self.side
-		Perimeter_of_a_circle=4(p)
+	def Perimeter_of_a_square(self):
+		side=self.side
+		Perimeter_of_a_square=4*side
+		return(Perimeter_of_a_square)
 
 
 class Rectangle:
 	def __init__(self,w,l):
-		self.w=0
-		self.l=0		
+		self.w=w
+		self.l=l		
 
 
-	def Area_of_a_rectangle(self,m,n):
-		m=self.w
-		n=self.l
-		A=m*n
+	def Area_of_a_rectangle(self):
+		w=self.w
+		l=self.l
+		Area_of_a_rectangle=w*l
+		return(Area_of_a_rectangle)
 
-	def Perimeter_of_a_rectangle(self,q,s):
-		q=self.w
-		s=sel.l
-		Perimeter_of_a_rectangle=2(q+s)
+	def Perimeter_of_a_rectangle(self):
+		w=self.w
+		l=self.l
+		Perimeter_of_a_rectangle=(2*w)+(2*l)
+		return(Perimeter_of_a_rectangle)
 
 
 class Sphere:
-	def __init__(self,sphere_radius):
-		self.sphere_radius=0
+	def __init__(self,radius):
+		self.radius=radius
 
-	def Sphere_SA(self,t):
-		t=self.sphere_radius
-		Sphere_SA=4*3.142*t*t
+	def Sphere_SA(self):
+		radius=self.radius
+		Sphere_SA=4*3.142*radius*radius
+		return(Sphere_SA)
 
-	def Sphere_Volume(self,v):
-		v=self.sphere_radius
-		Sphere_Volume=3/4(3.142*v*v*v)	
+	def Sphere_Volume(self):
+		radius=self.radius
+		Sphere_Volume=(4/33.142)*(radius*radius*radius)
+		return(Sphere_Volume)	
 
 
 
